b6db50283ef667626c01539e25cd6e36e8d2b126
Squeemos <56849503+Squeemos@users.noreply.github.com>
2023-01-31 10:07:01 -0800

    Try converting ANFIS to new method
    I think this version might be a lot better than the previous version. Plus it worked when changing the batch size, number of rules, input and output dimensions. In my head it makes sense, but we'll need to double check.

--- testing.py
+++ testing.py
@@ -5,42 +5,49 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from torch import optim
-from torchviz import make_dot
 
 from models.modules import ANFIS, DQN
 from models.utils import get_n_params
 
 def main() -> int:
-    batch_size = 2
-    n_rules = 5
-    out_dim = 2
-    in_dim = 4
+    batch_size = 12
+    n_rules = 100
+    out_dim = 5
+    in_dim = 7
     inp = torch.randn(batch_size, in_dim) * 2
 
     layer = nn.Linear(in_dim, out_dim)
-    act = nn.ReLU()
 
     outp = layer(inp)
-    outp = act(outp)
     print("in", inp.shape)
     print("net out", outp.shape)
 
     centers = torch.randn(out_dim, n_rules)
     widths = torch.randn(out_dim, n_rules)
-    params = torch.randn(out_dim, n_rules)
+    params = torch.randn(out_dim, in_dim)
+    biases = torch.randn(out_dim, in_dim)
 
     outputs = outp.unsqueeze(-1).expand(-1, -1, n_rules)
 
-    test = torch.exp(-((outputs - centers) / widths)**2)
+    # sqrt(2pi)
+    test = (1 / (widths * 2.50662827463)) * torch.exp(-((outputs - centers) / widths)**2 / 2)
     print("expanded out", outputs.shape)
     print("rule eval", test.shape)
 
     rule_eval = test / test.sum(dim=-1, keepdim=True)
     print("rule normed", rule_eval.shape)
 
-    print(rule_eval.shape)
+    i = inp.unsqueeze(-1).expand(-1, -1, n_rules).permute(0, 2, 1)
+    print("augmented input", i.shape)
 
+    defuzz = (rule_eval @ i)
+    print("input * rules", defuzz.shape)
 
+    j = params * defuzz + biases
+    print("params * rules", j.shape)
+
+    out = j.sum(dim=-1)
+    print("final output", out.shape)
 
 if __name__ == "__main__":
     raise SystemExit(main())
